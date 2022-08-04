7fb5b31a65dc59a99d6f8cf9bacc32dddb1f8ed0
Matheus Dos Reis Polato <99278233+MatheusDRPolato@users.noreply.github.com>
2022-08-04 17:25:09 -0300

    Add files via upload

---
+++ basexaltura.py
@@ -0,0 +1,5 @@
+base = int(input("Qual sera a base menor desejada?"))
+Base = int(input("Qual sera a base maior desejada?"))
+altura = int(input("Qual sera a altura desejada?"))
+a = ((Base + base)/2)*altura
+print(a)
